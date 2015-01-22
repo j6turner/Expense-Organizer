a59e8b295ad7f4fdb0ddce05af78960a1f16a519
Julie Turner & Andy Uppendahl <student@epicodus.com>
2015-01-22 15:05:57 -0800

    Add category_id instance variable.

--- lib/expense.rb
+++ lib/expense.rb
@@ -1,12 +1,13 @@
 class Expense
 
-  attr_reader(:id, :description, :amount, :date)
+  attr_reader(:id, :description, :amount, :date, :category_id)
 
   define_method(:initialize) do |attributes|
     @id = attributes.fetch(:id)
     @description = attributes.fetch(:description)
     @amount = attributes.fetch(:amount)
     @date = attributes.fetch(:date)
+    @category_id = attributes.fetch(:category_id)
   end
 
   define_singleton_method(:all) do
@@ -17,14 +18,16 @@ class Expense
       description = expense.fetch("description")
       amount = expense.fetch("amount").to_f()
       date = expense.fetch("date")
-      expenses.push(Expense.new({ :id => id, :description => description, :amount => amount, :date => date }))
+      category_id = expense.fetch("category_id").to_i()
+      expenses.push(Expense.new({ :id => id, :description => description,
+        :amount => amount, :date => date, :category_id => category_id }))
     end
     expenses
   end
 
   define_method(:save) do
-    result = DB.exec("INSERT INTO expenses (description, amount, date) " \
-      "VALUES ('#{@description}', #{@amount}, '#{@date}') RETURNING id;")
+    result = DB.exec("INSERT INTO expenses (description, amount, date, category_id) " \
+      "VALUES ('#{@description}', #{@amount}, '#{@date}', #{@category_id}) RETURNING id;")
     @id = result.first().fetch("id").to_i()
   end
 
@@ -32,7 +35,8 @@ class Expense
     (id() == another_expense.id()) &&
       (description() == another_expense.description()) &&
         (amount() == another_expense.amount()) &&
-          (date() == another_expense.date())
+          (date() == another_expense.date()) &&
+            (category_id() == another_expense.category_id())
   end
 
 end
--- spec/expense_spec.rb
+++ spec/expense_spec.rb
@@ -12,7 +12,7 @@ describe(Expense) do
     it("adds an expense to the table of expenses") do
       test_expense =
         Expense.new({ :id => nil, :description => "groceries",
-          :amount => 52.50, :date => "2015-01-22" })
+          :amount => 52.50, :date => "2015-01-22", :category_id => 3 })
       test_expense.save()
       expect(Expense.all()).to(eq([test_expense]))
     end
@@ -22,11 +22,57 @@ describe(Expense) do
     it("ensures two expenses with equal descriptions, amounts, " \
       "and dates are treated as equal") do
     expense1 = Expense.new({ :id => nil, :description => "groceries",
-      :amount => 52.50, :date => "2015-01-22" })
+      :amount => 52.50, :date => "2015-01-22", :category_id => 3 })
     expense2 = Expense.new({ :id => nil, :description => "groceries",
-      :amount => 52.50, :date => "2015-01-22"})
+      :amount => 52.50, :date => "2015-01-22", :category_id => 3 })
     expect(expense1).to(eq(expense2))
     end
   end
+
+  describe("#id") do
+    it("returns the id for an expense") do
+      test_expense =
+        Expense.new({ :id => nil, :description => "groceries",
+          :amount => 52.50, :date => "2015-01-22", :category_id => 3 })
+      test_expense.save()
+      expect(test_expense.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe("#description") do
+    it("returns the description for an expense") do
+      test_expense =
+        Expense.new({ :id => nil, :description => "groceries",
+          :amount => 52.50, :date => "2015-01-22", :category_id => 3 })
+      expect(test_expense.description()).to(eq("groceries"))
+    end
+  end
+
+  describe("#amount") do
+    it("returns the amount for an expense") do
+      test_expense =
+        Expense.new({ :id => nil, :description => "groceries",
+          :amount => 52.50, :date => "2015-01-22", :category_id => 3 })
+      expect(test_expense.amount()).to(eq(52.50))
+    end
+  end
+
+  describe("#date") do
+    it("returns the date for an expense") do
+      test_expense =
+        Expense.new({ :id => nil, :description => "groceries",
+          :amount => 52.50, :date => "2015-01-22", :category_id => 3 })
+      expect(test_expense.date()).to(eq("2015-01-22"))
+    end
+  end
+
+  describe("#category_id") do
+    it("returns the category id for an expense") do
+      test_expense =
+        Expense.new({ :id => nil, :description => "groceries",
+          :amount => 52.50, :date => "2015-01-22", :category_id => 3 })
+        expect(test_expense.category_id()).to(eq(3))
+      end
+    end
 
 end
